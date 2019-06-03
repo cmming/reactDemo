b42bd4ad3d6ab942d09f4fab081bc8ef089f6abc
cmming <2942257072@qq.com>
2019-06-03 08:47:54 +0800

    1.语言

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import store from './store/index'
 import {Provider} from 'react-redux'
+import { LocaleProvider } from 'antd';
+import {IntlProvider, addLocaleData} from 'react-intl';
+import zhCN from './locales/zh-CN.';  //导入 i18n 配置文件,需要手动创建并填入语言转换信息
+import enUS from './locales/en-US';
+import en from 'react-intl/locale-data/en';
+import zh from 'react-intl/locale-data/zh';
 import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -9,6 +15,11 @@ import * as serviceWorker from './serviceWorker';
 // import Admin from './pages/layout/index'
 
 import Router from './router/index'
+
+const langMap = {
+    'zh': zhCN,
+    'en': enUS
+};
 
 ReactDOM.render(
     <Provider store={store}>
